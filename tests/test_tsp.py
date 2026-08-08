14396b7a72b31f563c0ba6da5a5a6fe84a4aa320
Max Watson <2.max.leo.watson@gmail.com>
2026-08-08 11:00:27 +0100

    written failing test for finding a route of nearest neighbours, now need to write code for the function

--- tests/test_tsp.py
+++ tests/test_tsp.py
@@ -1,4 +1,4 @@
-from main import calculate_distance, calculate_route_length, find_nearest_city
+from main import calculate_distance, calculate_route_length, find_nearest_city, nearest_neighbour_route
 
 def test_calculate_distance():
     result = calculate_distance((0, 0), (3, 4))
@@ -19,3 +19,10 @@ def test_find_nearest_city():
     result = find_nearest_city(current_city, unvisited_cities)
 
     assert result == (2, 0)
+
+
+def test_nearest_neighbour_route():
+    cities = [(0, 0), (2, 0), (5, 0), (10, 0)]
+    result = nearest_neighbour_route(cities)
+
+    assert result == [(0, 0), (2, 0), (5, 0), (10, 0)]
